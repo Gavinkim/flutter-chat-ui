aa2601b5e266e6bcf25e42ea09aa69989742609c
Gavin kim <dreamworld612@gmail.com>
2020-04-05 02:47:16 +0900

    fluter chat ui tutorial done

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chatui/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatUI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Color(0xFFFEF9EB),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
